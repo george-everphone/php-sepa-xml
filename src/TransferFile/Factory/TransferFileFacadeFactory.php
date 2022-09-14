151acfd46a8e3beb366e0262538384643ea8f78b
George Ionita <george.ionita@everphone.de>
2022-09-14 15:28:55 +0100

    grouping

--- src/TransferFile/Factory/TransferFileFacadeFactory.php
+++ src/TransferFile/Factory/TransferFileFacadeFactory.php
@@ -38,9 +38,9 @@ class TransferFileFacadeFactory
      *                                            The first 8 or 11 characters of <Msgld> must match the BIC of the
      *                                            Instructing Agent. The rest of the field can be freely defined.
      */
-    public static function createDirectDebit(string $uniqueMessageIdentification, string $initiatingPartyName, string $painFormat = 'pain.008.002.02'): CustomerDirectDebitFacade
+    public static function createDirectDebit(string $uniqueMessageIdentification, string $initiatingPartyName, string $painFormat = 'pain.008.002.02', string $grouping): CustomerDirectDebitFacade
     {
-        $groupHeader = new GroupHeader($uniqueMessageIdentification, $initiatingPartyName);
+        $groupHeader = new GroupHeader($uniqueMessageIdentification, $initiatingPartyName, false, $grouping);
 
         return self::createDirectDebitWithGroupHeader($groupHeader, $painFormat);
     }
@@ -50,9 +50,9 @@ class TransferFileFacadeFactory
         return new CustomerDirectDebitFacade(new CustomerDirectDebitTransferFile($groupHeader), new CustomerDirectDebitTransferDomBuilder($painFormat));
     }
 
-    public static function createCustomerCredit(string $uniqueMessageIdentification, string $initiatingPartyName, string $painFormat = 'pain.001.002.03'): CustomerCreditFacade
+    public static function createCustomerCredit(string $uniqueMessageIdentification, string $initiatingPartyName, string $painFormat = 'pain.001.002.03', string $grouping): CustomerCreditFacade
     {
-        $groupHeader = new GroupHeader($uniqueMessageIdentification, $initiatingPartyName);
+        $groupHeader = new GroupHeader($uniqueMessageIdentification, $initiatingPartyName, false, $grouping);
 
         return self::createCustomerCreditWithGroupHeader($groupHeader, $painFormat);
     }
